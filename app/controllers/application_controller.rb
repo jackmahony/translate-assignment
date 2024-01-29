619cd6d3fbefab20373f2e495501da7479ce811f
Jack Mahony <jackmahonyy@gmail.com>
2024-01-29 17:57:33 +1000

    Added French language support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+    before_action :set_locale
+
+
+    protected
+
+        def after_sign_in_path_for(resource)
+            set_locale_from_cookie
+            super
+        end
+
+        def set_locale
+            I18n.locale = params[:locale] || cookies[:locale] || I18n.default_locale
+            cookies[:locale] = I18n.locale if params[:locale].present?
+        end
+
+        def set_locale_from_cookie
+            I18n.locale = cookies[:locale] || I18n.default_locale
+        end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def locale_link(locale)
+        url_for(params.permit(:locale).merge(locale: locale))
+    end
 end
